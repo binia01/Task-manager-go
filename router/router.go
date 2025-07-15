7e77e789e83c5d3d1cef6b2c5c1890fac58a5c9f
binia01 <biniabrha012@gmail.com>
2025-07-15 13:32:40 +0300

    Add router setup for task management API endpoints

---
+++ router/router.go
@@ -0,0 +1,16 @@
+package router
+
+import(
+	"task-manager-go/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine{
+	router := gin.Default()
+	router.GET("/tasks", controllers.GetTasks)
+	router.GET("/tasks/:id", controllers.GetTaskById)
+	router.POST("/tasks", controllers.CreateTask)
+	router.PUT("/tasks/:id", controllers.UpdateTask)
+	router.DELETE("/tasks/:id", controllers.DeleteTask)
+	return router
+}
