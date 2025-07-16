a63f37511abda2b6d45f073fa1eeeb79be325e3d
binia01 <biniabrha012@gmail.com>
2025-07-16 15:04:27 +0300

    Refactor task management functions to use MongoDB for data storage and retrieval

--- data/task_service.go
+++ data/task_service.go
@@ -1,69 +1,126 @@
 package data
 
 import (
-	"time"
+	"context"
+	"fmt"
+	"log"
 
 	"task-manager-go/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var tasks = []models.Task{
-    {ID: "1", Title: "Task 1", Description: "First task", DueDate: time.Now(), Status: "Pending"},
-    {ID: "2", Title: "Task 2", Description: "Second task", DueDate: time.Now().AddDate(0, 0, 1), Status: "In Progress"},
-    {ID: "3", Title: "Task 3", Description: "Third task", DueDate: time.Now().AddDate(0, 0, 2), Status: "Completed"},
-}
-
+var db = connectDb()
+var collection = db.Database("Task-Database").Collection("Tasks")
 
 func GetAllTasks() []models.Task {
+	findOptions := options.Find()
+
+	var tasks []models.Task
+
+	cur, err := collection.Find(context.TODO(), bson.D{}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for cur.Next(context.TODO()) {
+		var elem models.Task
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tasks = append(tasks, elem)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	cur.Close(context.TODO())
 	return tasks
 }
 
 func GetTaskById(id string) (*models.Task, bool) {
-	for _, t := range tasks {
-		if t.ID == id {
-			return &t, true
-		}
+	var task models.Task
+
+	err := collection.FindOne(context.TODO(), bson.M{"id": id}).Decode(&task)
+	if err != nil {
+		return nil, false
 	}
-	return nil, false
+	return &task, true
 }
 
 func UpdateTask(id string, updatedTask models.Task) (*models.Task, bool) {
-	for i, t := range tasks {
-		if t.ID == id {
-			if updatedTask.Title != "" {
-				tasks[i].Title = updatedTask.Title
-			}
-			if updatedTask.Description != "" {
-				tasks[i].Description = updatedTask.Description
-			}
-			if !updatedTask.DueDate.IsZero() {
-				tasks[i].DueDate = updatedTask.DueDate
-			}
-			if updatedTask.Status != "" {
-				tasks[i].Status = updatedTask.Status
-			}
-			return &tasks[i], true
-		}
+	filter := bson.M{"id": id}
+	update := bson.M{
+		"$set": bson.M{
+			"title":       updatedTask.Title,
+			"description": updatedTask.Description,
+			"due_date":    updatedTask.DueDate,
+			"status":      updatedTask.Status,
+		},
 	}
-	return nil, false
+	_, err := collection.UpdateOne(context.TODO(), filter, update)
+
+	if err != nil {
+		return nil, false
+	}
+
+	var task models.Task
+	err = collection.FindOne(context.TODO(), filter).Decode(&task)
+	if err != nil {
+		return nil, false
+	}
+	return &task, true
 }
 
 func CreateTask(task models.Task) bool {
-	for _, t := range tasks {
-		if t.ID == task.ID {
-			return false
-		}
+	// Check if a task with the same ID already exists
+	var existingTask models.Task
+	err := collection.FindOne(context.TODO(), bson.M{"id": task.ID}).Decode(&existingTask)
+	if err == nil {
+		// Task with the same ID already exists
+		return false
 	}
-	tasks = append(tasks, task)
-	return true
+
+	// Proceed to insert the new task
+	_, err = collection.InsertOne(context.TODO(), task)
+	return err == nil
 }
 
 func DeleteTask(id string) (*models.Task, bool) {
-	for i, t := range tasks {
-		if t.ID == id {
-			deleted := tasks[i]
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			return &deleted, true
-		}
+	var task models.Task
+	err := collection.FindOne(context.TODO(), bson.M{"id": id}).Decode(&task)
+	if err != nil {
+		return nil, false
 	}
-	return nil, false
+
+	_, err = collection.DeleteOne(context.TODO(), bson.M{"id": id})
+	if err != nil {
+		return nil, false
+	}
+	return &task, true
+}
+
+func connectDb() *mongo.Client {
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb+srv://<>username:<password>@cluster0.tj8um.mongodb.net/?retryWrites=true&w=majority")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	return client
 }
