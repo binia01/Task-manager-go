f5760924f66eef6468127a607339c23791748360
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-07-28 19:32:06 +0300

    Remove unused database connection function from task repository

--- Repositories/task_repository.go
+++ Repositories/task_repository.go
@@ -104,19 +104,3 @@ func (tr *taskRepository) DeleteTask(id string) (*Domain.Task, error) {
 	}
 	return &task, nil
 }
-
-func connectDb() *mongo.Client {
-	clientOptions := options.Client().ApplyURI("mongodb+srv://<username>:<password>@cluster0.tj8um.mongodb.net/?retryWrites=true&w=majority")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Connected to MongoDB!")
-	return client
-}
