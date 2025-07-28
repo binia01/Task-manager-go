939c2379d750696e43168df2ce8bf594c4be9b1e
Binyam Abrha Abay <176568730+binia01@users.noreply.github.com>
2025-07-28 20:02:13 +0300

    Refactor MongoDB connection to use environment variables for URI and JWT secret

--- Delivery/main.go
+++ Delivery/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 	routers "task-manager-go/Delivery/router"
 	"task-manager-go/Infrastructure"
 	"task-manager-go/Repositories"
@@ -15,7 +16,12 @@ import (
 
 func main() {
 
-	clientOptions := options.Client().ApplyURI("mongodb+srv://<username>:<password>@cluster0.tj8um.mongodb.net/?retryWrites=true&w=majority")
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI not set in environment")
+	}
+
+	clientOptions := options.Client().ApplyURI(mongoURI)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
@@ -32,10 +38,15 @@ func main() {
 	userCollection := client.Database("Task-Database").Collection("Users")
 
 	passwordService := Infrastructure.NewPasswordService()
-	jwtSecret := []byte("wN7z@JrV3uK!dXp2qT$eGh8yF9cLb6mZ")
+
+	jwtSecret := os.Getenv("MONGODB_URI")
+	if jwtSecret == "" {
+		log.Fatal("Jwt_secret not set in environment")
+	}
+
 	jwtService := Infrastructure.NewJWTService(string(jwtSecret))
 
-	authMiddleware := Infrastructure.AuthMiddleware(jwtSecret)
+	authMiddleware := Infrastructure.AuthMiddleware([]byte(jwtSecret))
 
 	taskRepo := Repositories.NewTaskRepository(taskCollection)
 	userRepo := Repositories.NewUserRepository(userCollection)
